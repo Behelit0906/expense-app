35ebe977a749d64dbc507e7ad282ac7240b30d16
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-07-12 15:52:34 -0500

    Adjustment of limit methods

--- app/Controllers/Expense.php
+++ app/Controllers/Expense.php
@@ -3,6 +3,7 @@
     use app\Classes\Controller;
     use app\Models\Expense as expenseModel;
     use app\Models\User;
+    use app\Classes\Validator;
 
     class Expense extends Controller{
 
@@ -27,28 +28,27 @@
             $expenses = $this->expense->limit($_SESSION['user_id'], $pointer, $amount);
             $totalExpenses = count($this->user->get_expenses());
 
+            $data['user-data'] = [
+                'user_name' => $this->user->name,
+                'photo' => $this->user->photo,
+            ];      
             $data['expenses'] = $expenses;
+            $data['categories'] = $this->categories();
             $data['total'] = $totalExpenses;
 
             $this->json_response('200', $data);
         }
 
-        
 
         private function categories(){
-            $categories = [];
             $expenses = $this->user->get_expenses();
             
-            $temp = [];
+            $categories = [];
             foreach($expenses as $expense){
                 $category = $expense->belongToCategory();
-                if(!array_key_exists($category->name, $temp)){
-                    $temp[$category->name] = $category; 
+                if(!array_key_exists($category->id, $categories)){
+                    $categories[$category->id] = $category->name; 
                 }
-            }
-
-            foreach($temp as $key => $value){
-                array_push($categories, $value);
             }
 
             return $categories;
@@ -61,6 +61,32 @@
             $data['total'] = $this->expense->totalExpensesByCategory($this->user->id, $category_id);
 
             $this->json_response('200', $data);
+        }
+
+        public function delete(){
+
+            $temp = $this->prepareValidations([
+                'id' => ['required','numeric'],
+            ]);
+
+
+            $validator = new Validator;
+            $errorMessages = $validator->validate($temp[0], $temp[1]);
+
+            if(count($errorMessages) > 0){
+                $this->json_response(400, ['messages' => $errorMessages]);
+            }
+
+            $id = $_POST['id'];
+            $isOk = $this->expense->delete($id);
+
+            if($isOk){
+                $this->json_response('201',['messages' => 'Expense deleted']);
+            }
+            else{
+                $this->json_response('400',['messages' => 'An unknown error occurred, try again later']);
+            }     
+
         }
 
 
